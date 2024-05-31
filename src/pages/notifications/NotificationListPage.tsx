ee5ecd3351563fc03a915ccb0d254109602a9356
Kyle <khang194591@gmail.com>
2024-05-31 17:32:02 +0700

    update

--- src/pages/notifications/NotificationListPage.tsx
+++ src/pages/notifications/NotificationListPage.tsx
@@ -1,4 +1,5 @@
 import { useList } from '@/api';
+import { axiosInstance } from '@/api/utils';
 import { INotificationResDto } from '@/libs';
 import { formatDate } from '@/libs/helpers';
 import {
@@ -11,23 +12,36 @@ import {
   Stack,
   Title,
 } from '@mantine/core';
-import { useNavigate } from '@tanstack/react-router';
+import { useNavigate, useRouter } from '@tanstack/react-router';
 
 function NotificationListPage() {
+  const router = useRouter();
   const navigate = useNavigate();
 
   const { data } = useList<INotificationResDto>({ resource: 'notifications' });
 
-  const handleNavigate = (data: INotificationResDto) => {
+  const handleNavigate = async (data: INotificationResDto) => {
+    await axiosInstance.put(`notification/mark-as-read/${data.id}`);
+
     if (data.requestId) {
       navigate({ to: '/requests/$id', params: { id: data.requestId } });
     }
+    if (data.contractId) {
+      navigate({ to: '/contracts/$id', params: { id: data.contractId } });
+    }
   };
+
+  const handleReadAll = async () => {
+    await axiosInstance.put('notifications/mark-as-read');
+    router.invalidate();
+  };
+
   return (
     <Stack p={'lg'} px={32}>
       <Group justify="space-between">
         <Title>Notifications</Title>
         <Box flex={1} />
+        <Button onClick={() => handleReadAll()}>Mark all as read</Button>
       </Group>
 
       <SimpleGrid cols={1} pb={24}>
@@ -40,7 +54,9 @@ function NotificationListPage() {
                   <p>{notification.content}</p>
                 </div>
                 <p>{formatDate(notification.createdAt)}</p>
-                <Button onClick={() => handleNavigate()}>View detail</Button>
+                <Button onClick={() => handleNavigate(notification)}>
+                  View detail
+                </Button>
               </div>
             </Card>
           </Indicator>
